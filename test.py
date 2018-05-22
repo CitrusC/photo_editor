c7d40405339cdcc2e3bf059463cba73f809fd2a4
Daisuke Inagaki <al16007@shibaura-it.ac.jp>
2018-05-22 15:20:52 +0900

    Refactored code.

--- test.py
+++ test.py
@@ -2,14 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication)
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.resize(600,400)
+        self.move(300, 300)
+        self.setWindowTitle('Simple')
+        self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
+    win=MainWindow()
     sys.exit(app.exec_())
