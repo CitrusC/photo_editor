af66ca290e292e0a0a97252b000dd9d69dab9f21
Daisuke Inagaki <al16007@shibaura-it.ac.jp>
2018-06-05 15:48:52 +0900

    change filter_sample.

--- sample/filter_sample/Filter.py
+++ sample/filter_sample/Filter.py
@@ -10,6 +10,29 @@ class Filter(metaclass=ABCMeta):
 
 class Nega(Filter):
     def apply(self, array):
+        print('run')
+        array = array.astype(np.float32)
         array[:, :, 0] = 255 - array[:, :, 0]
         array[:, :, 1] = 255 - array[:, :, 1]
         array[:, :, 2] = 255 - array[:, :, 2]
+        return array
+
+
+class Brightness(Filter):
+    brightness = 0
+
+    def set_parameter(self, brightness):
+        print(brightness)
+        self.brightness = brightness
+
+    def apply(self, array):
+        array[:, :, 0] = array[:, :, 0] + self.brightness
+        array[:, :, 1] = array[:, :, 1] + self.brightness
+        array[:, :, 2] = array[:, :, 2] + self.brightness
+        array = np.clip(array, 0, 255)
+        return array
+
+
+class DoNothing(Filter):
+    def apply(self, array):
+        pass
--- sample/filter_sample/Main.py
+++ sample/filter_sample/Main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QLCDNumber, QSlider
 from PyQt5.QtGui import QPixmap
 import Filter
 import numpy as np
@@ -13,21 +14,39 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.nega = Filter.Nega()
+        self.br = Filter.Brightness()
         self.show()
 
     def initUI(self):
         self.resize(600, 500)
         self.setWindowTitle('filter_sample')
         # 画像を読み込んで、arrayにセット
-        self.array = np.array(Image.open("sample.jpg").convert("RGBA"))
+        self.array = np.array(Image.open("sample.jpg").convert("RGBA"), np.float32)
+        print(self.array.dtype)
         # 画像を読み込んで、ラベルに貼り付け
         pixmap = QPixmap("sample.jpg")
         self.lbl = QLabel(self)
         self.lbl.setPixmap(pixmap)
         # フィルタ適用用のボタンを作って、関数にリンク
-        btn1 = QPushButton("Button 1", self)
+        btn1 = QPushButton("Nega", self)
         btn1.move(400, 50)
-        btn1.clicked.connect(self.button_clicked)
+        btn1.clicked.connect(self.button_clicked1)
+        btn1 = QPushButton("Brightness", self)
+        btn1.move(400, 100)
+        btn1.clicked.connect(self.button_clicked2)
+        # 数字のウィジェットを作成
+        lcd = QLCDNumber(self)
+        lcd.resize(150, 100)
+        lcd.move(400, 150)
+        # スライダーウィジェット作成
+        self.sld = QSlider(Qt.Horizontal, self)
+        self.sld.resize(150, 30)
+        self.sld.move(400, 300)
+
+        # スライダーを変更すると、数字も切り替わる
+        self.sld.valueChanged.connect(lcd.display)
+        self.sld.sliderReleased.connect(self.release_mouse)
 
     def ndarray_to_qpixmap(self, image):
         qimage = QtGui.QImage(image.data, image.shape[1], image.shape[0], image.shape[1] * 4,
@@ -39,14 +58,22 @@ class MainWindow(QWidget):
         self.lbl.setPixmap(self.ndarray_to_qpixmap(array))
         self.update()
 
-    def button_clicked(self):
-        # ネガフィルタを掛ける場合
-        # オブジェクト生成
-        nega = Filter.Nega()
+    def button_clicked1(self):
         # フィルタを適用する
-        nega.apply(self.array)
+        self.array = self.nega.apply(self.array)
         # 画面更新
-        self.update_image(self.array)
+        print(self.array.dtype)
+        self.update_image(self.array.astype(np.uint8))
+        print(self.array.dtype)
+
+    def button_clicked2(self):
+        # フィルタを適用する
+        self.array = self.br.apply(self.array)
+        # 画面更新
+        self.update_image(self.array.astype(np.uint8))
+
+    def release_mouse(self):
+        self.br.set_parameter(self.sld.value())
 
 
 if __name__ == '__main__':
