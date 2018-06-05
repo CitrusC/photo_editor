542d8ceb9dc89b5e00ceaf9fcc7ef045ac21de4b
tkt101 <kenjimitana@gmail.com>
2018-06-05 14:32:42 +0900

    comment

---
+++ sample/file save sample/sample5.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+
+class App(QWidget):
+
+
+
+
+
+
+
+
+    getSaveFileName(self,'savefile','/home')
