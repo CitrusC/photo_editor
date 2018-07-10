021f64160086532fd9794eaf5818f946cd3ac537
imushun <39510810+imushun@users.noreply.github.com>
2018-07-10 14:33:58 +0900

    File open is Completed. filter list sorting is being coded.

---
+++ sample/Filter Menu/Filter.py
@@ -0,0 +1,93 @@
+from abc import ABCMeta, abstractmethod
+import numpy as np
+from PyQt5.QtWidgets import QLabel, QHBoxLayout, QSlider
+from PyQt5.QtCore import Qt
+
+
+class Filter(metaclass=ABCMeta):
+    def __init__(self):
+        self.before_image_id = None
+        self.after_image_id = None
+        self.isUpdate = False
+        self.parent = None
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    @abstractmethod
+    def apply(self, array):
+        pass
+
+    @abstractmethod
+    def get_name(self):
+        pass
+
+    @abstractmethod
+    def get_layout(self):
+        pass
+
+
+class Nega(Filter):
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, array):
+        array[:, :, 0] = 255 - array[:, :, 0]
+        array[:, :, 1] = 255 - array[:, :, 1]
+        array[:, :, 2] = 255 - array[:, :, 2]
+        return array
+
+    def get_name(self):
+        return 'Nega filter {} {}'.format(self.before_image_id, self.after_image_id)
+
+    def get_layout(self):
+        label = QLabel(self.get_name())
+        layout = QHBoxLayout()
+        layout.addWidget(label)
+        return layout
+
+
+class Brightness(Filter):
+    def __init__(self):
+        super().__init__()
+        self.brightness = 0
+
+    def set_parameter(self, brightness):
+        self.isUpdate = True
+        self.brightness = brightness
+        self.parent.parent_list.update_filter(self)
+
+    def apply(self, array):
+        array[:, :, 0] = array[:, :, 0] + self.brightness
+        array[:, :, 1] = array[:, :, 1] + self.brightness
+        array[:, :, 2] = array[:, :, 2] + self.brightness
+        array = np.clip(array, 0, 255)
+        return array
+
+    def get_layout(self):
+        label = QLabel(self.get_name())
+        self.slider = QSlider(Qt.Horizontal, self.parent)
+        self.slider.setRange(-255, 255)
+        self.slider.setValue(self.brightness)
+        self.slider.sliderReleased.connect(self.release_mouse)
+        layout = QHBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(self.slider)
+        return layout
+
+    def release_mouse(self):
+        self.set_parameter(self.slider.value())
+
+    def get_name(self):
+        return 'Brightness filter{} {}'.format(self.before_image_id, self.after_image_id)
+
+
+class DoNothing(Filter):
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, array):
+        pass
+
+    def get_name(self):
+        return 'DoNothing filter'
---
+++ sample/Filter Menu/History.py
@@ -0,0 +1,94 @@
+class History:
+
+    def __init__(self, array):
+        self.filter_list = [[]]
+        self.image_list = [None]
+        self.image_count = 0
+        self.count = 0
+        self.redo_max = 0
+        self.current = [0]
+        self.image_list[0] = array.copy()
+
+    def add_filter(self, filter_):
+        self.next_filter()
+        if len(self.filter_list[self.count]) == 0:
+            filter_.before_image_id = self.image_count
+        else:
+            filter_.before_image_id = self.filter_list[self.count][-1].after_image_id
+        self.next_image()
+        filter_.after_image_id = self.image_count
+        self.filter_list[self.count].append(filter_)
+
+    def remove_filter(self, f):
+        self.next_filter()
+        rm_index = self.filter_list[self.count].index(f)
+        self.filter_list[self.count].remove(f)
+        for i in range(rm_index, len(self.filter_list[self.count])):
+            if i == 0:
+                self.filter_list[self.count][i].before_image_id = 0
+            else:
+                self.filter_list[self.count][i].before_image_id = self.filter_list[self.count][i - 1].after_image_id
+            self.next_image()
+            self.filter_list[self.count][i].after_image_id = self.image_count
+        return self.filter_list[self.count]
+
+    def swap(self, filters):
+        self.next_filter()
+        for i in range(len(self.filter_list[self.count])):
+            if self.filter_list[self.count][i] is filters[i]:
+                ud_index = i
+                continue
+            else:
+                ud_index += 1
+                break
+        self.filter_list[self.count] = filters.copy()
+        for i in range(ud_index, len(self.filter_list[self.count])):
+            self.filter_list[self.count][i].before_image_id = self.filter_list[self.count][i - 1].after_image_id
+            self.next_image()
+            self.filter_list[self.count][i].after_image_id = self.image_count
+        return self.filter_list[self.count]
+
+    def update_filter(self, f):
+        self.next_filter()
+        ud_index = self.filter_list[self.count].index(f)
+        for i in range(ud_index, len(self.filter_list[self.count])):
+            self.filter_list[self.count][i].before_image_id = self.filter_list[self.count][i - 1].after_image_id
+            self.next_image()
+            self.filter_list[self.count][i].after_image_id = self.image_count
+        return self.filter_list[self.count]
+
+    def undo(self):
+        if (self.count != 0):
+            self.count -= 1
+            self.redo_max += 1
+        return (self.image_list[self.current[self.count]], self.filter_list[self.count], self.count != 0)
+
+    def redo(self):
+        if self.redo_max > 0:
+            self.count += 1
+            self.redo_max -= 1
+        return (self.image_list[self.current[self.count]], self.filter_list[self.count], self.redo_max > 0)
+
+    def get_filter_list(self):
+        return self.filter_list[self.count].copy()
+
+    def next_filter(self):
+        self.count += 1
+        self.filter_list.append([])
+        self.filter_list[self.count] = self.filter_list[self.count - 1].copy()
+        self.current.append(self.current[self.count - 1])
+        self.redo_max = 0
+
+    def next_image(self):
+        self.image_count += 1
+        self.image_list.append(None)
+
+    def apply(self):
+        self.next_filter()
+        for i, f in enumerate(self.filter_list[self.count]):
+            if self.image_list[f.after_image_id] is not None:
+                continue
+            self.image_list[f.after_image_id] = f.apply(self.image_list[f.before_image_id].copy())
+        else:
+            self.current[self.count] = f.after_image_id
+        return (self.image_list[self.current[self.count]], self.filter_list[self.count])
